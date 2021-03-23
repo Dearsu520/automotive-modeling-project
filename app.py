fc1bd681a180fef5c14b014832edfe20b2f5a2f9
Yijing Su <dearyijingsu@hotmail.com>
2021-03-22 19:26:08 -0600

    Added routes to flask api

--- app.py
+++ app.py
@@ -14,5 +14,29 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+## Route to show available APIs
+@app.route("/api")
+def api():
+    return ("<ul>" +
+    "<li><p3>/: Landing page</p3></li>" + 
+    "<li><p3>/home: Landing page</p3></li>" +
+    "<li><p3>/api: page to show all the APIs</p3></li>" +
+    "<li>/api/priceInquery: Route to send and receive inqueries and results</p3></li>"
+    "</ul>")
+
+## Route for posting user inqueries
+@app.route("/api/priceInquery", methods=["GET", "POST"])
+def priceInquery():
+    if request.method == "POST":
+        vehicleType = request.form["vehicleType"]
+        model = request.form["model"]
+        year = request.form["year"]
+        fuelType = request.form["fuelType"]
+        condition = request.form["condition"]
+        paintColor = request.form["paintColor"]
+
+    ## TODO: The machine learning model needs to be loaded to finish this
+    return jsonify({"message": "Model will be loaded shortly"}), 200
+
 if __name__ == "__main__":
     app.run(debug=True)
