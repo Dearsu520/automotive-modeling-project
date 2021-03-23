8afb7b8a916bdbd225efc01068a029675b4e9e52
Yijing Su <dearyijingsu@hotmail.com>
2021-03-23 15:14:01 -0600

    update app.py. Added code to be able to load the machine learning model

--- app.py
+++ app.py
@@ -4,39 +4,56 @@ from flask import (
     jsonify,
     request,
     redirect)
+import pickle
+import numpy as np
 
-## Set up flask
+# Set up flask
 app = Flask(__name__)
 
-## Route to the landing page
+# Load the machine learning model TODO finalize the pickle file name
+model = pickle.load(open('model.pkl','rb')) 
+
+# Route to the landing page
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template("index.html")
 
-## Route to show available APIs
+# Route to show available APIs
 @app.route("/api")
 def api():
     return ("<ul>" +
     "<li><p3>/: Landing page</p3></li>" + 
     "<li><p3>/home: Landing page</p3></li>" +
     "<li><p3>/api: page to show all the APIs</p3></li>" +
-    "<li>/api/priceInquery: Route to send and receive inqueries and results</p3></li>"
+    "<li><p3>/api/priceInquery: Route to send and receive inqueries and results</p3></li>"
     "</ul>")
 
-## Route for posting user inqueries
-@app.route("/api/priceInquery", methods=["GET", "POST"])
+# Route for posting user inqueries
+@app.route("/api/priceInquery", methods=["POST"])
 def priceInquery():
-    if request.method == "POST":
-        vehicleType = request.form["vehicleType"]
-        model = request.form["model"]
-        year = request.form["year"]
-        fuelType = request.form["fuelType"]
-        condition = request.form["condition"]
-        paintColor = request.form["paintColor"]
+    # Obtain the request parameters
+    vehicleType = request.form["vehicleType"]
+    year = request.form["year"]
+    model = request.form["model"]
+    fuelType = request.form["fuelType"]
+    condition = request.form["condition"]
+    paintColor = request.form["paintColor"]
 
-    ## TODO: The machine learning model needs to be loaded to finish this
-    return jsonify({"message": "Model will be loaded shortly"}), 200
+    # Obtain prediction from the model
+    prediction = model.predict(
+        np.array([
+            vehicleType,
+            year,
+            model,
+            fuelType,
+            condition,
+            paintColor
+        ])
+    )
+
+    result = prediction[0] ## TODO should I do it like this?
+    return jsonify(result)
 
 if __name__ == "__main__":
     app.run(debug=True)
