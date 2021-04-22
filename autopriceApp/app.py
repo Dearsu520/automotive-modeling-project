0654ab28e87656c6318b4338d7d3c308c193f181
kk-deng <71301799+kk-deng@users.noreply.github.com>
2021-04-22 17:03:04 -0400

    Updated Tableau part

--- autopriceApp/app.py
+++ autopriceApp/app.py
@@ -10,6 +10,13 @@ import numpy as np
 import math 
 import pandas as pd
 
+# Define functions for round prediction value to obtain price range 
+def roundup(x):
+    return int(math.ceil(x / 1000)) * 1000
+
+def rounddown(x):
+    return int(math.floor(x/1000))*1000
+
 # Set up flask
 app = Flask(__name__)
 
@@ -24,14 +31,14 @@ def home():
     return render_template("index.html")
 
 # Route to show available APIs
-@app.route("/api")
-def api():
-    return ("<ul>" +
-    "<li><p3>/: Landing page</p3></li>" + 
-    "<li><p3>/home: Landing page</p3></li>" +
-    "<li><p3>/api: page to show all the APIs</p3></li>" +
-    "<li><p3>/api/priceInquery: Route to send and receive inqueries and results</p3></li>"
-    "</ul>")
+# @app.route("/api")
+# def api():
+#     return ("<ul>" +
+#     "<li><p3>/: Landing page</p3></li>" + 
+#     "<li><p3>/home: Landing page</p3></li>" +
+#     "<li><p3>/api: page to show all the APIs</p3></li>" +
+#     "<li><p3>/api/priceInquery: Route to send and receive inqueries and results</p3></li>"
+#     "</ul>")
 
 
 # Route for posting user inqueries
@@ -43,7 +50,7 @@ def priceInquery():
     
     try:
         # Obtain the request parameters
-        print(request.form["vehicleType"])
+        # print(request.form["vehicleType"])
         vehicleType = request.form["vehicleType"] or "truck"
         manufacturer = request.form["carManufacturer"] or "honda" 
         year = request.form["year"] or "2006"
@@ -64,16 +71,9 @@ def priceInquery():
 
         prediction = model.predict(input_df)[0]
     except:
-        input_data = ['NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA','NA', 'NA', 'NA']
+        input_data = ['NA'] * 10
         # If some of the values are not provided, return 0
         prediction = 0
-
-    # round prediction value to obtain price range 
-    def roundup(x):
-        return int(math.ceil(x / 1000)) * 1000
-
-    def rounddown(x):
-        return int(math.floor(x/1000))*1000
 
     # prediction = prediction #fake value for testing purpose
     prediction_range = [0, 0] #placeholder for ranges
